141f6eb015215771d03b24e9bed6d3baba8a9760
smyn95 <tlsalduszz@gmail.com>
2023-07-16 20:13:10 +0900

    게시물 카드 컴포넌트 추가

--- src/components/featuredPosts.tsx
+++ src/components/featuredPosts.tsx
@@ -6,7 +6,7 @@ export default async function FeaturedPosts() {
 
     return (
         <section>
-            <h2>Featured Posts</h2>
+            <h2 className="my-2 text-2xl font-bold">최근 게시물</h2>
             <PostsGrid posts={posts} />
         </section>
     );
--- src/components/postsGrid.tsx
+++ src/components/postsGrid.tsx
@@ -1,4 +1,5 @@
 import { Post } from '@/service/posts';
+import PostCard from './postCard';
 
 type Props = {
     posts: Post[];
@@ -6,9 +7,11 @@ type Props = {
 
 export default function PostsGrid({ posts }: Props) {
     return (
-        <ul>
+        <ul className="grid">
             {posts.map((post) => (
-                <li key={post.path}>{post.title}</li>
+                <li key={post.path}>
+                    <PostCard post={post} />
+                </li>
             ))}
         </ul>
     );
