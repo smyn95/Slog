cb54f7e2d69c414c919f22b606a4633b4d0a1cd5
smyn95 <tlsalduszz@gmail.com>
2023-07-16 19:51:55 +0900

    게시물 노출 조건 함수 추가

--- src/components/featuredPosts.tsx
+++ src/components/featuredPosts.tsx
@@ -1,10 +1,13 @@
+import { getFeaturedPosts } from '@/service/posts';
 import PostsGrid from './postsGrid';
 
-export default function FeaturedPosts() {
+export default async function FeaturedPosts() {
+    const posts = await getFeaturedPosts();
+
     return (
         <section>
             <h2>Featured Posts</h2>
-            <PostsGrid />
+            <PostsGrid posts={posts} />
         </section>
     );
 }
--- src/components/postsGrid.tsx
+++ src/components/postsGrid.tsx
@@ -1,7 +1,15 @@
-export default function PostsGrid() {
+import { Post } from '@/service/posts';
+
+type Props = {
+    posts: Post[];
+};
+
+export default function PostsGrid({ posts }: Props) {
     return (
-        <>
-            <div></div>
-        </>
+        <ul>
+            {posts.map((post) => (
+                <li key={post.path}>{post.title}</li>
+            ))}
+        </ul>
     );
 }
