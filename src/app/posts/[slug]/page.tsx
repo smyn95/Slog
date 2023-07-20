7a3c8b0be16a6b1581a4d6bdb978c7a9472d1e33
smyn95 <tlsalduszz@gmail.com>
2023-07-20 11:40:11 +0900

    디테일 페이지 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,17 @@
-export default function Post() {
+import { getPostData } from '@/service/posts';
+
+type Props = {
+    params: {
+        slug: string;
+    };
+};
+
+export default async function PostPage({ params: { slug } }: Props) {
+    const post = await getPostData(slug);
     return (
         <>
-            <div>post slug</div>
+            <h1>{post.title}</h1>
+            <pre>{post.content}</pre>
         </>
     );
 }
