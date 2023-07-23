c28a5eaf5f152a7d8a56da745c1b7513812c0247
smyn95 <tlsalduszz@gmail.com>
2023-07-23 21:02:51 +0900

    소개 페이지 추가

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,32 @@
-export default function Contact() {
+import ContactForm from '@/components/contactForm';
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
+
+const LINKS = [
+    { icon: <AiFillGithub />, url: '' },
+    { icon: <AiFillLinkedin />, url: '' },
+    { icon: <AiFillYoutube />, url: '' }
+];
+
+export default function ContactPage() {
     return (
-        <>
-            <div>CONTACT page</div>
-        </>
+        <section className="flex flex-col items-center">
+            <h2 className="my-2 text-3xl font-bold">Contact Me</h2>
+            <p>info@dream-coding.com</p>
+            <ul className="flex gap-4 my-2">
+                {LINKS.map((link, index) => (
+                    <a
+                        key={index}
+                        href={link.url}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="text-5xl hover:text-yellow-400"
+                    >
+                        {link.icon}
+                    </a>
+                ))}
+            </ul>
+            <h2 className="my-8 text-3xl font-bold">Or Send me an email</h2>
+            <ContactForm />
+        </section>
     );
 }
--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,7 +1,18 @@
+import Profile from '@/components/pofile/Page';
+
+const TITLE_CLASS = 'text-2xl font-bold text-gray-800 my-2';
 export default function Me() {
     return (
         <>
-            <div>ME page</div>
+            <Profile />
+            <section className="p-8 m-8 text-center bg-gray-100 shadow-lg">
+                <h2 className={TITLE_CLASS}>Who Am I?</h2>
+                <p>FE</p>
+                <h2 className={TITLE_CLASS}>Career</h2>
+                <p>Liche</p>
+                <h2 className={TITLE_CLASS}>Skills</h2>
+                <p>React</p>
+            </section>
         </>
     );
 }
