e9dbe8a0c5d41f3060123d386adba384fe554b54
smyn95 <tlsalduszz@gmail.com>
2023-07-30 15:58:16 +0900

    함수 중복요청 제거
    - getAllPosts함수 중복 요청 제거를 위한 cache사용
    - 최근 게시물은 정적 페이지로 수정

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import AdjacentPostCard from '@/components/adjacentPostCard';
 import PostContent from '@/components/postContent';
-import { getPostData } from '@/service/posts';
+import { getFeaturedPosts, getPostData } from '@/service/posts';
 import { Metadata } from 'next';
 import Image from 'next/image';
 
@@ -36,4 +36,11 @@ export default async function PostPage({ params: { slug } }: Props) {
             </section>
         </article>
     );
+}
+
+export async function generateStaticParams() {
+    const posts = await getFeaturedPosts();
+    return posts.map(( post ) => ({
+            slug: post.path,
+    }));
 }
